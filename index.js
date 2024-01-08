c8a75faf4341821b412e935a3798841c34b517a4
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-01-08 23:41:38 +0000

    Book card text creation

--- index.js
+++ index.js
@@ -5,6 +5,40 @@ function Book() {
     this.author = author;
 }
 
+function createBookCard(title, author, pages, read) {
+    let booksContainer = document.getElementsByClassName('books-container')[0];
+    
+    let card = document.createElement('div');
+    card.classList.add('book-card');
+    booksContainer.appendChild(card);
+
+    let bookTitle = document.createElement('span');
+    bookTitle.classList.add('card-property', 'book-title');
+    bookTitle.textContent = title;
+    card.appendChild(bookTitle);
+
+    let bookAuthor = document.createElement('span');
+    bookAuthor.classList.add('card-property');
+    bookAuthor.textContent = `By: ${author}`;
+    card.appendChild(bookAuthor);
+
+    let bookPages = document.createElement('span');
+    bookPages.classList.add('card-property');
+    bookPages.textContent = `Total Pages: ${pages}`;
+    card.appendChild(bookPages);
+
+    let bookStatus = document.createElement('span');
+    bookStatus.classList.add('card-property');
+    bookStatus.textContent = `Status: ${read ? 'Read' : 'Not Read'}`;
+    card.appendChild(bookStatus);
+}
+
 function addBookToLibrary() {
 
 }
+
+function displayBooks() {
+
+}
+
+createBookCard('Frankenstein', 'Mary Shelley', 288, true);
