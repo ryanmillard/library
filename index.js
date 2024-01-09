ecea9d07af38c8d35c094fcb7690d41faf110df2
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-01-09 23:00:58 +0000

    Add book dialog

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const addBookButton = document.getElementById('add-book-btn');
+const overlay = document.getElementById('overlay');
+
 const myLibrary = [];
 
-function Book() {
+function Book(title, author) {
     this.title = title;
     this.author = author;
 }
@@ -24,6 +27,20 @@ function createBookCard(title, author, pages, read) {
     createCardProperty(`By ${author}`);
     createCardProperty(`Total Pages: ${pages}`);
     createCardProperty(`Status: ${read ? 'Read' : 'Not Read'}`);
+
+    let btnContainer = document.createElement('div');
+    btnContainer.classList.add('card-btn-container');
+    card.append(btnContainer);
+
+    let readBtn = document.createElement('button');
+    readBtn.classList.add('card-btn', 'read-card-btn');
+    readBtn.textContent = 'Mark Read';
+    btnContainer.append(readBtn);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('card-btn', 'delete-card-btn');
+    deleteBtn.textContent = 'Delete';
+    btnContainer.append(deleteBtn);
 }
 
 function addBookToLibrary() {
@@ -35,3 +52,21 @@ function displayBooks() {
 }
 
 createBookCard('Frankenstein', 'Mary Shelley', 288, true);
+
+function closeAddBookDialog() {
+    let addBookDialog = document.getElementById('add-book-dialog');
+    let overlay = document.getElementById('overlay');
+    addBookDialog.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+addBookButton.addEventListener('click', () => {
+    let addBookDialog = document.getElementById('add-book-dialog');
+    let overlay = document.getElementById('overlay');
+    addBookDialog.style.display = 'flex';
+    overlay.style.display = 'block';
+});
+
+overlay.addEventListener('click', () => {
+    closeAddBookDialog();
+});
