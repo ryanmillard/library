c7cfacc0824c3b0c9076674516b56311ccd280f7
ryanmillard <110338235+ryanmillard@users.noreply.github.com>
2024-01-28 20:30:02 +0000

    Object constructor to class syntax

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const addBookForm = document.forms["add_book"];
 
 var myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;   
+    }
 }
 
 function getBookIndex(title, author) {
